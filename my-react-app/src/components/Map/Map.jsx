5b03542a8413ff06949f127897010ec57eab955e
Ayanna Aziz <ayannaaziz@Ayannas-MBP.attlocal.net>
2024-04-28 20:55:46 -0500

    barebones for main  components

--- my-react-app/src/components/Map/Map.jsx
+++ my-react-app/src/components/Map/Map.jsx
@@ -1,1 +1,9 @@
-my-react-app/src/components/Header/List
+import React from "react";
+
+const Header = () => {
+    return(
+        <h1> Map </h1>
+    );
+}
+
+export default Map;
---
+++ my-react-app/src/components/Place Details/PlaceDetails.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <h1> Place Details </h1>
+    );
+}
+
+export default PlaceDetails;
