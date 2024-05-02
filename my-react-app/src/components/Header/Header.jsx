fc254f3c83e82fb24eab883411d5a6af03484573
Ayanna Aziz <ayannaziz1919@gmail.com>
2024-05-01 21:10:08 -0500

    built out app bar and fixed typos

--- my-react-app/src/components/Header/Header.jsx
+++ my-react-app/src/components/Header/Header.jsx
@@ -4,7 +4,24 @@ import { AppBar, Toolbar, Typography, InputBase, Box } from'@material-ui/core';
 
 const Header = () => {
     return(
-        <h1> Header </h1>
+        <AppBar position="static"> 
+        <Toolbar className={classes.toolbar}>
+            <Typography variant="h5" className={classes.title}>
+                Travel Advisor
+            </Typography>
+            <Box display="flex">
+                <Typography variant="h6" className={classes.title}>
+                    Explore New Places
+                </Typography>
+                <Autocomplete>
+                    
+                </Autocomplete>
+
+            </Box>
+
+        </Toolbar>
+
+        </AppBar>
     );
 };
 
