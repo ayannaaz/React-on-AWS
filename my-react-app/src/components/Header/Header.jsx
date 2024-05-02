45357ef785260e0599dcefa4d52aa9db411755da
Ayanna Aziz <ayannaziz1919@gmail.com>
2024-05-02 07:42:14 -0500

    styling

--- my-react-app/src/components/Header/Header.jsx
+++ my-react-app/src/components/Header/Header.jsx
@@ -1,28 +1,30 @@
 import React from "react";
-import { Autocomplete } from '@react-google-maps/api';
-import { AppBar, Toolbar, Typography, InputBase, Box } from'@material-ui/core';
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
+import SearchIcon from "@mui/icons-material";
 
 const Header = () => {
-    return(
-        <AppBar position="static"> 
-        <Toolbar className={classes.toolbar}>
-            <Typography variant="h5" className={classes.title}>
-                Travel Advisor
-            </Typography>
-            <Box display="flex">
-                <Typography variant="h6" className={classes.title}>
-                    Explore New Places
-                </Typography>
-                <Autocomplete>
-                    
-                </Autocomplete>
-
-            </Box>
-
-        </Toolbar>
-
-        </AppBar>
-    );
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore New Places
+          </Typography>
+          <Autocomplete>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+            </div>
+          </Autocomplete>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
